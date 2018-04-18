a0e61840b13ac9243c0049eab6c20616d4c8530c
JakeOh <jake.oh.lecture@gmail.com>
2018-04-18 17:57:12 +0900

    add ex06_01 function

--- src/edu/kotlinessentials/ch06/ex06_01.kt
+++ src/edu/kotlinessentials/ch06/ex06_01.kt
@@ -2,4 +2,19 @@ package edu.kotlinessentials.ch06
 
 fun main(args: Array<String>) {
     println("Ex06-01")
+
+    println(min1(1, 2))
+    println(min2(1, 2))
+    println(min1(param1 = 10, param2 = 2)) // named argument
 }
+
+// fun funtionName(parameter: Type = defaultValue, ... ): ReturnType { // body }
+fun min1(param1: Int, param2: Int): Int {
+    return if (param1 < param2) param1 else param2
+}
+
+// omit return type: Kotlin's type inference
+fun min2(param1: Int, param2: Int) =
+        if (param1 < param2) param1 else param2
+
+// default value of parameters
