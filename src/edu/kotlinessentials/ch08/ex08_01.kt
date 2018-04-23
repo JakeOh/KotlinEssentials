10d3532e32b9efd031cd93d6ff41a804b4d032b6
JakeOh <jake.oh.lecture@gmail.com>
2018-04-23 15:11:19 +0900

    added lambda example

--- src/edu/kotlinessentials/ch08/ex08_01.kt
+++ src/edu/kotlinessentials/ch08/ex08_01.kt
@@ -7,6 +7,23 @@ fun main(args: Array<String>) {
     println(sum9(10, 20))
     println(sum10(100, 200))
     println(sum11(1000, 2000))
+
+    println()
+    println(funcExec1(11, 22, sum4))
+    println(funcExec1(11, 22, { x, y -> x * y }))
+
+    println()
+    val listOfLambdas = ArrayList<(Int, Int) -> Int>()
+    listOfLambdas.add(sum5)
+
+    val subtract = { x: Int, y: Int -> x - y }
+    listOfLambdas.add(subtract)
+
+    listOfLambdas.add({ x, y -> x * y})
+
+    for (i in 0 .. (listOfLambdas.size - 1)) {
+        println(listOfLambdas[i](1, 2))
+    }
 }
 
 fun sum1(x: Int, y: Int): Int {
@@ -31,3 +48,8 @@ val sum9: (Int, Int) -> Int = { x, y -> x + y }
 val sum10 = { x: Int, y: Int -> x + y }
 
 val sum11 = { x: Int, y: Int -> println("$x + $y = ${x+y}");x + y }
+
+// higher-order functions
+fun funcExec1(x: Int, y: Int, func: (Int, Int) -> Int): Int {
+    return func(x, y)
+}
